5989fb4cb274c07db067241ed505890d3636bee8
Melvin Thomas <melvin.thomas@bigbinary.com>
2023-09-06 19:55:36 +0530

    Added ability to display list of tasks

---
+++ app/javascript/src/components/Dashboard/index.jsx
@@ -0,0 +1,56 @@
+import React, { useState, useEffect } from "react";
+
+import { isNil, isEmpty, either } from "ramda";
+
+import taskApi from "apis/tasks";
+import Container from "components/Container";
+import PageLoader from "components/PageLoader";
+import Table from "components/Tasks/Table";
+
+const Dashboard = () => {
+  const [tasks, setTasks] = useState([]);
+  const [loading, setLoading] = useState(true);
+
+  const fetchTasks = async () => {
+    try {
+      const {
+        data: { tasks },
+      } = await taskApi.list();
+      setTasks(tasks);
+      setLoading(false);
+    } catch (error) {
+      logger.error(error);
+      setLoading(false);
+    }
+  };
+
+  useEffect(() => {
+    fetchTasks();
+  }, []);
+
+  if (loading) {
+    return (
+      <div className="w-screen h-screen">
+        <PageLoader />
+      </div>
+    );
+  }
+
+  if (either(isNil, isEmpty)(tasks)) {
+    return (
+      <Container>
+        <h1 className="text-xl leading-5 text-center">
+          You have no tasks assigned 😔
+        </h1>
+      </Container>
+    );
+  }
+
+  return (
+    <Container>
+      <Table data={tasks} />
+    </Container>
+  );
+};
+
+export default Dashboard;
